50256d1118839c670dcb386dc413738ae19d47f3
buterajacques1 <jbutera702@gmail.com>
2023-04-07 15:15:27 +0200

    testing a documentation of a module

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
-"""First api with flask and python"""
+"""Flask server (variable app)
+"""
 
-import os
+
+from flask import Flask, jsonify
 from models import storage
+from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, make_response
-from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
-app.url_map.strict_slashes = False  # allow /api/v1/states/ and /api/v1/states
-
-host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-port = os.getenv("HBNB_API_PORT", 5000)
-
-
-@app.errorhandler(404)
-def error_not_found(self):
-    """404 error but return empty dict"""
-    return make_response(jsonify({"error": "Not found"}), 404)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(*args, **kwargs):
-    """close storage"""
+def downtear(self):
+    '''Status of your API'''
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify('error='Not found'), 404
+
+
 if __name__ == "__main__":
-    """Flask Boring App"""
-    # print(app.url_map)
-    app.run(host=host, port=port)
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
