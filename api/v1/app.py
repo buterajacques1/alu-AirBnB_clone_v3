2384e427e8dc37cee70783e49ef51f8a7e4c1007
buterajacques1 <jbutera702@gmail.com>
2023-04-07 16:02:11 +0200

    empty init file at the root of api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,21 +1,24 @@
-# api/v1/app.py
+#!/usr/bin/python3
+"""
+This module creates an instance of Flask and registers the blueprint app_views
+"""
 
-from flask import Flask
-from models import storage
+from flask import Flask, jsonify
 from api.v1.views import app_views
+from models import storage
+
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """Close the database"""
+def teardown_db(exception):
+    """Teardown database"""
     storage.close()
 
 
 if __name__ == "__main__":
-    from os import environ
-    host = environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = environ.get('HBNB_API_PORT', '5000')
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
