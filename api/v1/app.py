61a7354ea398372a841c43f1c4b3683e740ee0c7
buterajacques1 <jbutera702@gmail.com>
2023-04-07 18:24:14 +0200

    not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
 """
-This module creates an instance of Flask and registers the blueprint app_views
+Flask app
 """
-
 from flask import Flask, jsonify
-from api.v1.views import app_views
 from models import storage
-import os
+from api.v1.views import app_views
+from flask_cors import CORS
+from os import environ
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Teardown database"""
+def teardown_app(exception):
     storage.close()
 
 
-if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
+@app.errorhandler(404)
+def page_not_found(e):
+    """handler for routes that don't exist"""
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == '__main__':
+    host = environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = environ.get("HBNB_API_PORT", 5000)
     app.run(host=host, port=port, threaded=True)
